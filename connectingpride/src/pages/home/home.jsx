2f08ec2b5c51f35cccb8d79fa215a38a6dbf49d8
ARES-79 <89227872+ARES-79@users.noreply.github.com>
2023-07-23 21:33:04 -0700

    NavBar, Header, Home Page

---
+++ connectingpride/src/pages/home/home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./home.css"
+import Header from "../../components/header/header";
+
+export default function Home() {
+
+    return (
+        <>
+            <Header/>
+            <div className="home">
+
+            </div>
+        </>
+    )
+
+}
